92702ffe6f5296de1f84d6bb50c8b5218284622a
laubonghaudoi <laubonghaudoi@icloud.com>
2026-02-02 13:53:31 -0800

    Deploy example app

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -6,7 +6,7 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 import type { UserConfig } from "vite";
 
 export default {
-	base: "./",
+	base: "/rime-react/",
 	plugins: [
 		react(),
 		viteStaticCopy({
